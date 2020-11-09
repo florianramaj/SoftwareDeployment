9c803962ab4e9b32cd551272761b5ef60d942653
florianramaj <ramaj.flo@gmail.com>
2020-11-09 13:25:35 +0100

    Commit 0911

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000;
 let appInsights = require("applicationinsights");
 appInsights
   .setup("aa6fb425-d1c2-4fca-bd5a-5115b190f2b4")
   .start()
 
-app.get('/test', (req, res) => {
-  console.log("hei");
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.render("index");
 })
 
 app.listen(port, () => {
