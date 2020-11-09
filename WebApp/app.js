8ef4233dcb88b20efe647c99f1384efbc0e25baa
florianramaj <ramaj.flo@gmail.com>
2020-11-09 13:33:07 +0100

    added

--- WebApp/app.js
+++ WebApp/app.js
@@ -4,7 +4,9 @@ const port = process.env.PORT||3000;
 let appInsights = require("applicationinsights");
 appInsights
   .setup("aa6fb425-d1c2-4fca-bd5a-5115b190f2b4")
-  .start()
+  .start();
+
+app.set("view engine", "jade");
 
 app.get('/', (req, res) => {
   res.render("index");
